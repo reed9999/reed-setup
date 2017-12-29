2cc7ba70eae7832c9032167c499c383ec48e4eec
Philip J. Reed <reed9999@uw.edu>
2017-12-29 12:52:35 -0800

    Tweak 04-pretrained to properly use the locally stored model.

--- OpenNMT/setup.sh
+++ OpenNMT/setup.sh
@@ -4,13 +4,16 @@
 #http://torch.ch/docs/getting-started.html
 ########################################
 
-git clone https://github.com/torch/distro.git ~/torch --recursive
-cd ~/code/torch; bash install-deps;
-./install.sh
+if [ "$1" == "--all" ]
+then
+	git clone https://github.com/torch/distro.git ~/torch --recursive
+	cd ~/code/torch; bash install-deps;
+	./install.sh
 
-source ~/.bashrc
-#Still something wrong with this transition
-# luarocks didn't work implying .bashrc hadn't really been sourced.
+	source ~/.bashrc
+	#Still something wrong with this transition
+	# luarocks didn't work implying .bashrc hadn't really been sourced.
+fi
 
 luarocks install tds
 luarocks install bit32 
