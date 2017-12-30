36adbc5f7e20314473c3ef0d0138ba2da962dba4
reed9999 <reed9999@uw.edu>
2017-12-30 15:33:33 -0800

    Committing to be ready to pull changes.

--- latex/1-diss-basics.sh
+++ latex/1-diss-basics.sh
@@ -1,13 +1,16 @@
 
+sudo apt install texmaker
 
 
-#NOTA BENE: This script isn't really in use. I needed a solution to compare two
+
+#NOTA BENE: The rest of this script isn't really in use. I needed a solution to compare two
 # LaTeX and liked what it says here: 
 # https://www.sharelatex.com/blog/2013/02/16/using-latexdiff-for-marking-changes-to-tex-documents.html
 # but so far it isn't working for me.
 
 ########################################
 #OLD STUFF
+
 
 #NO, don't do this per texlive # apt-get install texlive
 
@@ -18,12 +21,14 @@
 
 #echo "########################################"
 echo "texlive with the Ubuntu Software Centre GUI does not work great."
+echo "Anyway, if texlive isn't working then at the moment I have no desire"
+echo "  to install it."
 #echo "Maybe there is a way to do this in a script, but I don't know it."
 #echo "########################################"
 
-which perl
-perl --version
-mkdir -p $HOME/Downloads/tmp
+# # which perl
+# # perl --version
+# # mkdir -p $HOME/Downloads/tmp
 # # wget -O $HOME/Downloads/tmp/latexdiff.zip http://mirrors.ctan.org/support/latexdiff.zip
 # # unzip $HOME/Downloads/tmp/latexdiff.zip -lv
 # # unzip $HOME/Downloads/tmp/latexdiff.zip -d $(which perl)
