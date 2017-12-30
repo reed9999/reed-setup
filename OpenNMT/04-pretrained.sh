f536e72b1d2e9ed88e2b77d5f8c1d21fd61ec375
Philip J. Reed <reed9999@uw.edu>
2017-12-29 23:55:03 -0800

    Add vim config files; much adaptation for istallation on second machine (HP laptop).

--- OpenNMT/04-pretrained.sh
+++ OpenNMT/04-pretrained.sh
@@ -11,9 +11,9 @@ mkdir -p ~/code/__local-large/OpenNMT/pretrained
 cd ~/code/__local-large/OpenNMT/pretrained
 PRETRAINED_URL=https://s3.amazonaws.com/opennmt-models/onmt_baseline_wmt15-all.en-de_epoch13_7.19_release.t7
 echo $PRETRAINED_URL
-wget $PRETRAINED_URL
+# # wget $PRETRAINED_URL
 MODEL=$PRETRAINED_DIR/onmt_baseline_wmt15-all.en-de_epoch13_7.19_release.t7
 cd ~/code/OpenNMT
-th translate.lua -model $MODEL -src data/src-test.txt -output pred.txt
+th translate.lua -model $MODEL -src ~/u/OpenNMT/data/src-test.txt -output ~/u/OpenNMT/pred.txt
 
 
