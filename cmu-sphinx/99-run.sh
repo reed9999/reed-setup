1780fdd459e08386c5ca2f4e6962dbc8cbd225fe
reed9999 <reed9999@uw.edu>
2018-01-13 10:34:40 -0800

    Corresponding to last commit, shell script changes.

--- cmu-sphinx/99-run.sh
+++ cmu-sphinx/99-run.sh
@@ -6,25 +6,38 @@
 # think of is that compiling ./hello_ps.c may have somehow put things in the 
 # right place?
 ################################################################################
+WORKING=~/u/cmu-sphinx
+INPUT=$WORKING/__input
+OUTPUT=$WORKING/__output
+EL_GR=$WORKING/__el-gr
 
-if 0 ; then
-  pocketsphinx_continuous -infile 17-11-06-19-51-56.wav > decode-result.txt
+echo "en: English test"
+echo "elr: resample Greek"
+read choice
+
+
+if [ $choice = "en" ] 
+then
+  pocketsphinx_continuous -infile $INPUT/17-11-06-19-51-56.wav > $OUTPUT/decode-result.txt
 
 #http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Main/16kHz_16bit/
-else
-  
-  ~/code/u/cmu-sphinx/96-resample.sh
+elif [ $choice = "elr" ] 
+then
+  echo "TODO fix to actually resample"
+  #~/code/u/cmu-sphinx/96-resample.sh
 
-array=(34 39 40 41 42 43)
-for number in "${array[@]}"
-do 
+  #array=(34 39 40 41 42 43)
+  array=(34)
+  for number in "${array[@]}"
+  do 
 
+  echo "Resampling $number"
 
-  pocketsphinx_continuous \
-  -hmm "/usr/local/share/pocketsphinx/model/el-gr/el-gr.cd_cont_5000" \
-  -lm "/usr/local/share/pocketsphinx/model/el-gr/el-gr.lm" \
-  -dict "/usr/local/share/pocketsphinx/model/el-gr/el-gr.dic" \
-  -infile ~/code/u/cmu-sphinx/el-gr/aero_iap-20150703-ftm/wav/resample-el-00$number.wav  > decode-el$number.txt
+    pocketsphinx_continuous \
+    -hmm "/usr/local/share/pocketsphinx/model/el-gr/el-gr.cd_cont_5000" \
+    -lm "/usr/local/share/pocketsphinx/model/el-gr/el-gr.lm" \
+    -dict "/usr/local/share/pocketsphinx/model/el-gr/el-gr.dic" \
+    -infile $EL_GR/aero_iap-20150703-ftm/wav/resample-el-00$number.wav  > $OUTPUT/decode-el$number.txt
 
 done
 fi
