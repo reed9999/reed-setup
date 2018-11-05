dccbd27aa2d4c199d84181821e6a7556069222ea
reed9999 <>
2018-11-05 13:39:21 -0800

    Helpful snippet to check a sha256 checksum now.

--- items/jetbrains.sh
+++ items/jetbrains.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
 # Script to install and configure the Jetbrains Toolbox
+# Truthfully I may just end up wanting to install the individual pieces 
+# programmatically anyway -- don't know if Toolbox has a CLI -- but it's still 
+# good to see the canonical places they should go. 
 
 if which jetbrains-toolbox ; then   #actually I don't know the command yet, just guessing.
-  echo "VS Code (code) appears to be installed."; exit 0; 
+  echo "Jetbrains Toolbox appears to be installed."
+  TOOLBOX_INSTALLED=true
 fi;
 
-echo "Not yet complete; come back to this one"
-exit 999
 
 # It's a weakness that the URL is HARDCODED but I figure it will autoupdate
 # itself once it's installed.
@@ -16,25 +19,33 @@ URL=https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.11.4269.tar.gz
 CHECKSUM_URL=https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.11.4269.tar.gz.sha256
 
 JETBRAINS_GLOB=jetbrains-toolbox*.gz
-if [ ! -e $JETBRAINS_GLOB ] && [ ! -e ~/Downloads/$JETBRAINS_GLOB ]; then 
-        wget $URL
-        wget $CHECKSUM_URL
+NEEDS_DOWNLOAD=true
+if [ -e ~/Downloads/$JETBRAINS_GLOB ] ; then 
+  echo "Jetbrains Toolbox downloaded .gz already exists and is in the right place."
+  NEEDS_DOWNLOAD=false
+fi;
+if [ -e $JETBRAINS_GLOB ] ; then 
+  echo "Jetbrains Toolbox downloaded .gz already exists; move it to Downloads."
+  mv $JETBRAINS_GLOB ~/Downloads
+  mv jetbrains*.sha256 ~/Downloads
+  NEEDS_DOWNLOAD=false
+fi 
+if [ "$NEEDS_DOWNLOAD" = true ] ; then 
+  echo "Will now download the toolbox gz file and checksum."
+  wget $URL
+  wget $CHECKSUM_URL
 else
-        echo "Jetbrains Toolbox file already exists"
+  echo "No need to download the toolbox gz file."
 fi;
 
-# See https://stackoverflow.com/questions/21956954/how-to-check-the-checksum-through-commandline
+# See https://help.ubuntu.com/community/HowToSHA256SUM
+SHA256=.sha256
 
-CHECKSUM=`cat jetbrains*.sha256`
-shasum jetbrains-toolbox*.gz |
-  awk "$1==$CHECKSUM{print'good to go'}"
-
-#2018-11-04 16:30:18 (7.13 MB/s) - ‘jetbrains-toolbox-1.11.4269.tar.gz.sha256’ saved [101/101]
-
-#awk: line 1: syntax error at or near ==
-
-
-mv jetbrains-toolbox*.gz ~/Downloads/
-
-
+pushd ~/Downloads
+result=`sha256sum -c $JETBRAINS_GLOB$SHA256|grep -c OK`
+if [ result != 1 ] ; then 
+  echo "Checksum did not check out! Aborting Jetbrains Toolbox install"
+  exit 999;
+fi;
+popd
 
