8bb67ebab5b995d4033ee0534937c90f22041e4a
reed9999 <>
2018-11-05 11:33:13 -0800

    New config and keybindings files for VS Code

--- items/vscode.sh
+++ items/vscode.sh
@@ -1,16 +1,19 @@
 if which code ; then 
-  echo "VS Code (code) appears to be installed."; exit 0; 
-fi;
-
-VSCODE_GLOB=vscode-current.deb
-
-if [ ! -e $VSCODE_GLOB ] && [ ! -e ~/Downloads/$VSCODE_GLOB ]; then 
-	wget -O vscode-current.deb \
-	       	https://go.microsoft.com/fwlink/?LinkID=760868
+  echo "VS Code (code) appears to be installed."; 
 else
-  echo "VS Code install .deb file already exists"
-fi;
-mv $VSCODE_GLOB ~/Downloads/
-sudo dpkg -i ~/Downloads/$VSCODE_GLOB 
-sudo apt install -f
+  VSCODE_GLOB=vscode-current.deb
 
+  if [ ! -e $VSCODE_GLOB ] && [ ! -e ~/Downloads/$VSCODE_GLOB ]; then 
+    wget -O vscode-current.deb \
+            https://go.microsoft.com/fwlink/?LinkID=760868
+  else
+    echo "VS Code install .deb file already exists"
+  fi;
+  mv $VSCODE_GLOB ~/Downloads/
+  sudo dpkg -i ~/Downloads/$VSCODE_GLOB 
+  sudo apt install -f
+fi;
+
+# Be conservative for now. Files I know I want to copy.
+cp  ~/.config/Code/User/*.json /tmp
+cp CONFIG-FILES/Code/User/*.json ~/.config/Code/User
