ec00b25deffc4ca4bb87cadceaf5b3630f166fe3
Philip J. Reed <>
2018-11-05 19:11:32 -0800

    Renamings associated with build_stuff_from_source Not everything in items/repos/ is actually a repo, but that was the most logical parking place. Other stuff should move to other places (probably the root of items) as needed. Successes: ['init', 'chromium-browser', 'vlc', 'lynx', 'redshift', 'p7zip-full', 'pidgin', 'anki', 'okular', 'git', 'vim', 'vscode', 'jetbrains', 'python-logilab-common', 'python-pip', 'python3-pip', 'clone_repos', 'tox', 'build_stuff_from_source', 'docky', 'golly'] Failures: ['tusk', 'ffmpeg', 'all_translation']

--- items/build_stuff_from_source.sh
+++ items/build_stuff_from_source.sh
@@ -1,13 +1,12 @@
+#!/bin/bash
 # This was a function in the 2-build-code.sh script and is worth trying to
 # extract out into an item in the new setup.  
+#---------------
 
-
-  #---------------
-  # BUILD FROM SOURCE
-  #---------------
+CODE_DIR=~/code
+CUSTOM_SCRIPT_DIR=~/reed-setup/items/repos
 build_stuff_from_source(){
-  #DRY violation -- mostly repeated from the part above, but not all
-  # Not sure whether to include econgraphs
+  #DRY violation -- mostly repeated from clone_repos
   array=(
   anki
   Theano 
@@ -17,13 +16,10 @@ build_stuff_from_source(){
   )
   for repo in "${array[@]}"
   do
-    cd ~/code/$repo/
-  #This is a horrible way to do this.
-  #Only one of the 2 following will run successfully. 
-    echo "Now making directory just in case it's still needed: $repo."
-    mkdir -p ~/u/$repo
-
+    pushd ~/code/$repo/
     echo "Now running custom Philip setup script for $repo."
-    ~/u/$repo/setup.sh 
+     
   done
 }
+
+build_stuff_from_source
